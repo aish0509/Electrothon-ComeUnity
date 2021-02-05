f1a97c7dae25373cf550e0bbcf13f70a0c41defc
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-02-06 01:55:52 +0530

    post file added

---
+++ lib/models/post.dart
@@ -0,0 +1,15 @@
+class Post {
+  String uid;
+  String desc;
+  String image;
+  String by;
+  String time;
+  String title;
+  Post(
+      {this.uid,
+      this.desc,
+      this.image,
+      this.by,
+      this.time,
+      this.title});
+}
