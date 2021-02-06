a4fc55dc51caf27c47eb27b6cefb9931ff65890c
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-02-06 15:08:43 +0530

    global imports added

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -2,7 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/user.dart';
+import 'package:flutter_application_1/services/comment_services.dart';
+import 'package:flutter_application_1/services/firebase_auth_service.dart';
+import 'package:flutter_application_1/services/model_services.dart';
+import 'package:flutter_application_1/services/post_services.dart';
 import 'package:flutter_application_1/services/upload_file_firebase.dart';
+import 'package:flutter_application_1/services/user_service.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 double defaultHeight = 896;
@@ -16,7 +21,12 @@ User userData;
 
 
 final UploadFileFirebase uploadFileFirebase = UploadFileFirebase.getInstance();
-
+final FirebaseAuthService firebaseAuthService =
+    FirebaseAuthService.getInstance();
+final UserService userService = UserService.getInstance();
+final PostService postService = PostService.getInstance();
+final CommentService commentService = CommentService.getInstance();
+final ModeService modeService = ModeService.getInstance();
 
 Widget showSnackbar(
   String text,
